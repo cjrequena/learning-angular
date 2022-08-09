01f23430e533766ae1f30302da5a4103a30a4113
cjrequena <cjrequena@protonmail.com>
2022-08-09 15:57:16 +0200

    --

--- 3-angular-directives/src/app/components/attribute-directives/ngclass/ngclass.component.ts
+++ 3-angular-directives/src/app/components/attribute-directives/ngclass/ngclass.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ngclass',
   templateUrl: './ngclass.component.html',
-  styleUrls: ['./ngclass.component.css']
+  styleUrls: ['./ngclass.component.css'],
 })
 export class NgclassComponent implements OnInit {
+  
+  public currentClasses: Record<string, boolean> = {};
+  public isSpecial: boolean = true;
+  public canSave: boolean = true;
+  public isUnchanged: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.setCurrentClasses();
   }
 
+  setCurrentClasses() {
+    // CSS classes: added/removed per current state of component properties
+    this.currentClasses =  {
+      saveable: this.canSave,
+      modified: !this.isUnchanged,
+      'p-3 mb-2 bg-info text-dark':  this.isSpecial
+    };
+  }
 }
--- 3-angular-directives/src/app/components/attribute-directives/ngstyle/ngstyle.component.ts
+++ 3-angular-directives/src/app/components/attribute-directives/ngstyle/ngstyle.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgstyleComponent implements OnInit {
 
+  public available: boolean = false;
+  public canSave:boolean = true;
+  public isUnchanged:boolean = true;
+  public isSpecial:boolean = true;
+
+
+  currentStyles: Record<string, string> = {};
+
   constructor() { }
 
   ngOnInit(): void {
+    this.setCurrentStyles();
+  }
+
+  setCurrentStyles() {
+    // CSS styles: set per current state of component properties
+    this.currentStyles = {
+      'font-style':  this.canSave      ? 'italic' : 'normal',
+      'font-weight': !this.isUnchanged ? 'bold'   : 'normal',
+      'font-size':   this.isSpecial    ? '24px'   : '12px',
+      'background-color': this.isSpecial ? 'black' : 'transparent',
+      'color': this.isSpecial ? 'white': ''
+    };
   }
 
 }
